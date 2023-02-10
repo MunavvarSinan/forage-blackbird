dcab7c6ca3c5fafe22b53094c353a08faa594528
Munavvar sinan k p <munavvarsinan01@gmail.com>
2023-02-10 10:25:45 +0530

    added repo url to commands.sh

--- commands.sh
+++ commands.sh
@@ -1,3 +1,5 @@
+# REPO_URL : https://github.com/MunavvarSinan/forage-blackbird
+
 1. create a react application using vite  - `npx create-vite-app <project-name>`
 2. push the code to github
 3. create a new branch - `git checkout -b update_logo`
@@ -6,4 +8,3 @@
 6. push the changes to github - `git push origin update_logo`
 7. create a pull request
 8. merge the pull request
- 
