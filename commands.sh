f80fa3307c154f6142d60222888a13c8a9531da8
Munavvar sinan k p <munavvarsinan01@gmail.com>
2023-02-10 10:23:38 +0530

    updated logo and link

---
+++ commands.sh
@@ -0,0 +1,9 @@
+1. create a react application using vite  - `npx create-vite-app <project-name>`
+2. push the code to github
+3. create a new branch - `git checkout -b update_logo`
+4. update the logo in the `public` folder
+5. commit the changes - `git commit -m "update logo"`
+6. push the changes to github - `git push origin update_logo`
+7. create a pull request
+8. merge the pull request
+ 
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <div>
+        <a href='https://www.propelleraero.com/dirtmate/' target='_blank'>
+          <img src='/vite.svg' className='logo' alt='Vite logo' />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+
+      <p className='read-the-docs'>
+        Click on the  logo to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App
